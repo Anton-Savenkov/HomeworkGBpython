08a426a59d27bdb88613a5d2feec68f02d30d776
Anton Savenkov <a_savi@icloud.com>
2022-12-01 00:08:03 +0300

    add task04 in homework003 and rename var task03

--- Homework003/task03.py
+++ Homework003/task03.py
@@ -5,9 +5,9 @@ num_a = float(input("Введите число A: "))
 num_b = float(input("Введите число B: "))
 num_c = float(input("Введите число C: "))
 
-def sum_func(a, b, c):
+def sum_func(arg_a, arg_b, arg_c):
     """Функция принимает три элемента и возвращает сумму двуух наибольших"""
-    list_num = (a, b, c)
+    list_num = (arg_a, arg_b, arg_c)
     first_num = max(list_num)
     second_num = max(i for i in list_num if i != first_num)
     return first_num + second_num
