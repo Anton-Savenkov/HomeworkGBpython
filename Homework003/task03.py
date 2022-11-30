aac621077d9ddfb9537e087c889789627bf2a4db
Anton Savenkov <a_savi@icloud.com>
2022-11-30 22:42:15 +0300

    add task03 in homework003

---
+++ Homework003/task03.py
@@ -0,0 +1,15 @@
+"""Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов."""
+
+num_a = float(input("Введите число A: "))
+num_b = float(input("Введите число B: "))
+num_c = float(input("Введите число C: "))
+
+def sum_func(a, b, c):
+    """Функция принимает три элемента и возвращает сумму двуух наибольших"""
+    list_num = (a, b, c)
+    first_num = max(list_num)
+    second_num = max(i for i in list_num if i != first_num)
+    return first_num + second_num
+
+print(f"Сумма двух наибольших аргументов = {sum_func(num_a, num_b, num_c)}")
